adc33260d82bdc2ab5f2b985240a722c7bf8b311
Natalia-Duzhak <72571464+Natalia-Duzhak@users.noreply.github.com>
2020-11-30 00:57:21 +0300

    Add files via upload

---
+++ UnitTest8.2/UnitTest8.2.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Projeect8.2/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest82
+{
+	TEST_CLASS(UnitTest82)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+			char str[5] = { 'a','\0' };
+
+			int  max=0;
+
+			 MaxLength(str);
+			Assert::AreEqual(max, 0);
+
+		}
+	};
+}
